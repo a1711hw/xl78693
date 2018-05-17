8a6f22b84bc5447465453c84a8aa52637ade2607
v.A1711_HW <31956296+a1711hw@users.noreply.github.com>
2018-05-17 15:35:28 +0800

    bug fix

--- fb.sh
+++ fb.sh
@@ -6,21 +6,21 @@ export PATH
 file=/etc/firewalld/ipsets/secure.xml
 log=/var/log/secure
 
-time=$(date -d "-1 day" +"%B %d")
-filename=$(echo ${file}| awk -F '/' '{print $NF}' |awk -F '.' '{print $1}')
-ip_list=`grep "${time}" ${log} |grep 'Failed password' |egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' |sort | uniq -c |awk '$1>=10 {print $2}'`
+time=$(date -d "-1 day" +"%b %d")
+filename=$(echo ${file} | awk -F '/' '{print $NF}' |awk -F '.' '{print $1}')
+ip_list=`grep "${time}" "${log}" |grep 'Failed password' |egrep -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' |sort | uniq -c |awk '$1>=5 {print $2}'`
 
 main(){
     if [ ! -s ${file} ];then
-        firewall-cmd --permanent --zone=public --new-ipset=${filename} --type=hash:ip
-        firewall-cmd --permanent --zone=public --add-rich-rule="rule source ipset=${drop_filename} drop"
+        firewall-cmd --permanent --zone=drop --new-ipset=${filename} --type=hash:ip
+        firewall-cmd --permanent --zone=drop --add-rich-rule="rule source ipset=${filename} drop"
     fi
 
     for i in `echo "${ip_list}"`
     do
         grep ${i} ${file} >/dev/null
         if [ $? -ne 0 ];then
-            firewall-cmd --permanent --zone=public --ipset=${filename} --add-entry=${i}
+            firewall-cmd --permanent --zone=drop --ipset=${filename} --add-entry=${i}
         fi
     done
     firewall-cmd --reload
